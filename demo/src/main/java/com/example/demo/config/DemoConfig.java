ddca4ceaf48aa495f5bd176e8af6ce08aac99ddb
Karol <karolkrawczyk8650@gmail.com>
2023-09-22 12:36:04 +0200

    change config in app

--- demo/src/main/java/com/example/demo/config/DemoConfig.java
+++ demo/src/main/java/com/example/demo/config/DemoConfig.java
@@ -1,17 +1,20 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "bean")
 public class DemoConfig {
+    @Value("${my.property.name}")
+    private String myPropertyValue;
 
     private String message = "a message that can be changed live";
     private String interval = "1";
 
     public String getMessage() {
-        return message;
+        return message + "    " + myPropertyValue;
     }
 
     public void setMessage(String message) {
